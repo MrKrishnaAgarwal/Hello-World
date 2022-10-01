09ecb7e15bc6487fcea253f2a6c5f92a5406b470
kunevi <rotherexram@gmail.com>
2022-10-01 10:37:22 +0200

    Add x86 Assembly and Brainf*ck to the list.

---
+++ main.asm
@@ -0,0 +1,19 @@
+section .tex
+    global _start
+
+section .data
+msg db  'Hello, world!',0xa
+len equ $ - msg         
+
+section .text
+
+_start:
+    mov edx,len ;message length
+    mov ecx,msg ;message to write
+    mov ebx,1   ;file descriptor (stdout)
+    mov eax,4   ;system call number (sys_write)
+    int 0x80    ;call kernel
+
+    mov ebx,0   ;process' exit code
+    mov eax,1   ;system call number (sys_exit)
+    int 0x80    ;call kernel - this interrupt won't return
